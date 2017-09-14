6fc36db8d729c4db3b858d7529d2e8fb52885956
Nick Roberson <roberson@grinnell.edu>
2017-09-14 13:45:24 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,35 +1,43 @@
 class MoviesController < ApplicationController
 
+  # defines movie parameters
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
+  # shows a specific movie 
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
+  # main function that runs the view
   def index
-    # displays the movies in a certain oreder depending on what is in 'params'
-    @sort = params[:sort_by]
+    # gets the ratings to be displayed
+    @all_ratings = Movie.get_ratings
+    # displays the movies based on what we are sorting by 
     display_movies
   end
 
+  # ? 
   def new
     # default: render 'new' template
   end
-
+  
+  # creates a movie and inserts in database
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
+  # edits a movie
   def edit
     @movie = Movie.find params[:id]
   end
 
+  # updates a movie in the database 
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
@@ -37,6 +45,7 @@ class MoviesController < ApplicationController
     redirect_to movie_path(@movie)
   end
 
+  # deletes a movie from the database 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
@@ -44,16 +53,27 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
   
+  # displays movies based on sorting parameters
   def display_movies
+    
+    # filter movies by rating
+    # if no ratings are provided, choose all movies
+    movies = []
+    if params[:ratings]
+      movies = Movie.where(rating: params[:ratings].keys)
+    else
+      movies = Movie.all
+    end 
+    
+    # display movies, sorting by some parameter
+    puts "Displaying movies . . ."
     @sort = params[:sort_by]
     if @sort == 'title'
-      puts "Sorting Movies by title . . ."
-      @movies = Movie.order(:title)
+      @movies = movies.order(:title)
     elsif @sort == 'release_date'
-      puts "Sorting Movies by release_date . . ."
-      @movies = Movie.order(:release_date)
+      @movies = movies.order(:release_date)
     else
-      @movies = Movie.all
-    end 
-  end 
+      @movies = movies
+    end
+  end
 end
